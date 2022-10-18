abd1ef2f5fe30794cf0876b62b07f73e435b1274
FLP <42muhammedmustafa@gmail.com>
2022-10-18 03:24:20 +0200

    initiated databse

--- src/server.js
+++ src/server.js
@@ -2,14 +2,22 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 8000;
+const db = require("../data/database");
 
-app.use(express.static(path.resolve("public")));
-app.use(express.static(path.resolve("music")));
-
-const server = app.listen(port, () => {
-  console.log(`server running on localhost: ${port}`);
-});
+app.use(express.static("public"));
+app.use(express.static("music"));
 
 app.get("/", function (req, res) {
   res.sendFile(path.resolve("index.html"));
+});
+
+app.use(function (error, req, res, next) {
+  // Default error handling function
+  // Will become active whenever any route / middleware crashes
+  console.log(error);
+  res.status(500).render("500");
+});
+
+db.connectToDatabase().then(function () {
+  app.listen(port);
 });
