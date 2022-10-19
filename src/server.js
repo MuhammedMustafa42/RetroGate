1700619be25113621a7043d29dfd2d58d6fd3e6d
FLP <42muhammedmustafa@gmail.com>
2022-10-19 07:15:22 +0200

    tweaked the ui design

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 8000;
-const db = require("../data/database");
+const db = require("./data/database");
 
 app.use(express.static("public"));
 app.use(express.static("music"));
