f0a2782cd3f3b5ef8f077c455bfb258ba9017788
FLP <42muhammedmustafa@gmail.com>
2022-10-20 18:59:20 +0200

    -

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,19 +1,17 @@
 const express = require("express");
-const mongodb = require("mongodb");
 const path = require("path");
 const db = require("../data/database");
-const ObjectId = mongodb.ObjectId;
 const router = express.Router();
 
-router.get("/", function (req, res) {
+router.get("/", function (_req, res) {
   res.sendFile(path.resolve("index.html"));
 });
 
 router.post("/users", async function (req, res) {
   const newUser = {
-    username: "req.body.username",
-    email: "req.body.email",
-    password: "req.body.password",
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
   };
   const result = await db.getDb().collection("users").insertOne(newUser);
   console.log(result);
