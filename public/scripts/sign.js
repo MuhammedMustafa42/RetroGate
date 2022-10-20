ce5040bf795ad29037ee6d4a1ecbf1ef034c4b04
FLP <42muhammedmustafa@gmail.com>
2022-10-20 15:03:02 +0200

    initiated sign up form

--- public/scripts/sign.js
+++ public/scripts/sign.js
@@ -1,7 +1,22 @@
 const openSignInBtn = document.getElementById("sign-in-button");
 const closeSignInBtn = document.getElementById("close-button");
 const modal = document.getElementById("sign-in");
+
+const openSignUpBtn = document.getElementById("sign-up-button");
+const closeSignUpBtn = document.getElementById("up-close-button");
+const upModal = document.getElementById("sign-up");
+
 const overlay = document.getElementById("overlay");
+
+function upOpenModal() {
+  upModal.classList.add("active");
+  overlay.classList.add("active");
+}
+
+function upCloseModal() {
+  upModal.classList.remove("active");
+  overlay.classList.remove("active");
+}
 
 function openModal() {
   modal.classList.add("active");
@@ -15,3 +30,6 @@ function closeModal() {
 
 openSignInBtn.addEventListener("click", openModal);
 closeSignInBtn.addEventListener("click", closeModal);
+
+openSignUpBtn.addEventListener("click", upOpenModal);
+closeSignUpBtn.addEventListener("click", upCloseModal);
