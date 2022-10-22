44f8f4bf7ddd53c422fd3929a3fdbbf205e967ed
FLP <42muhammedmustafa@gmail.com>
2022-10-22 14:47:20 +0200

    added pw hashing

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,16 +2,24 @@ const express = require("express");
 const path = require("path");
 const db = require("../data/database");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const pepper = process.env.BCRYPT_PASSWORD;
+const saltRounds = process.env.SALT_ROUNDS;
 
 router.get("/", function (_req, res) {
   res.sendFile(path.resolve("index.html"));
 });
 
 router.post("/users", async function (req, res) {
+  const hash = bcrypt.hashSync(
+    req.body.password + pepper,
+    parseInt(saltRounds)
+  );
+
   const newUser = {
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: hash,
   };
   const result = await db.getDb().collection("users").insertOne(newUser);
   console.log(result);
