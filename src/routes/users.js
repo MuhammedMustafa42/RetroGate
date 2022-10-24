eede700dcd907eaf60fbca0d550e1761aaa9cfc8
FLP <42muhammedmustafa@gmail.com>
2022-10-24 11:07:37 +0200

    sign in bugs

--- src/routes/users.js
+++ src/routes/users.js
@@ -10,6 +10,7 @@ router.get("/", function (_req, res) {
   res.sendFile(path.resolve("index.html"));
 });
 
+//route for creating users (sign up)
 router.post("/users", async function (req, res) {
   const hash = bcrypt.hashSync(
     req.body.password + pepper,
@@ -25,6 +26,30 @@ router.post("/users", async function (req, res) {
   console.log(result);
   console.log(newUser);
   res.redirect("/");
+});
+
+//route for authentication (sign in)
+router.get("/users", async function (req, res) {
+  const input = {
+    email: req.body.emailin,
+    password: req.body.passwordin,
+  };
+
+  console.log(input);
+
+  const user = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: input.email });
+
+  console.log(user);
+
+  if (user) {
+    if (bcrypt.compareSync(input.password + pepper, user.password)) {
+      return user;
+    }
+  }
+  return null;
 });
 
 module.exports = router;
